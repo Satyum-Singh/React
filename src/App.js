fb829c455984a009e35e56273c2c2dcd214e2b19
Satyum Singh Pathania <satyumspathania@gmail.com>
2023-10-26 12:05:24 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>This is an unordered list</h1>
+      <ul>
+        <li> Item 1 </li>
+        <li> Item 2 </li>
+        <li> Item 3 </li>
+      </ul>
+    </div>
+  );
+}
